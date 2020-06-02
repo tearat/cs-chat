61d9457815540df46b75bbd721caef82c3088e75
teraaret <raiiliiat@yandex.ru>
2020-06-02 23:53:01 +0500

    Works with server now

--- ExampleSQLApp/MainForm.Designer.cs
+++ ExampleSQLApp/MainForm.Designer.cs
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button_exit = new System.Windows.Forms.Button();
             this.listLabels = new System.Windows.Forms.ListBox();
-            this.label_informer = new System.Windows.Forms.Label();
             this.textBox_name = new System.Windows.Forms.TextBox();
             this.label_name = new System.Windows.Forms.Label();
             this.label_message = new System.Windows.Forms.Label();
             this.textBox_message = new System.Windows.Forms.TextBox();
             this.button_send = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button_exit
@@ -43,7 +44,7 @@
             this.button_exit.AutoSize = true;
             this.button_exit.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_exit.Location = new System.Drawing.Point(663, 313);
+            this.button_exit.Location = new System.Drawing.Point(699, 461);
             this.button_exit.Name = "button_exit";
             this.button_exit.Padding = new System.Windows.Forms.Padding(20, 5, 20, 5);
             this.button_exit.Size = new System.Drawing.Size(109, 40);
@@ -54,26 +55,19 @@
             // 
             // listLabels
             // 
+            this.listLabels.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listLabels.FormattingEnabled = true;
-            this.listLabels.Location = new System.Drawing.Point(12, 37);
+            this.listLabels.ItemHeight = 23;
+            this.listLabels.Location = new System.Drawing.Point(25, 37);
             this.listLabels.Name = "listLabels";
-            this.listLabels.Size = new System.Drawing.Size(760, 212);
+            this.listLabels.Size = new System.Drawing.Size(783, 326);
             this.listLabels.TabIndex = 1;
             this.listLabels.TabStop = false;
-            // 
-            // label_informer
-            // 
-            this.label_informer.AutoSize = true;
-            this.label_informer.Location = new System.Drawing.Point(12, 9);
-            this.label_informer.Name = "label_informer";
-            this.label_informer.Size = new System.Drawing.Size(24, 13);
-            this.label_informer.TabIndex = 2;
-            this.label_informer.Text = "n/a";
             // 
             // textBox_name
             // 
             this.textBox_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_name.Location = new System.Drawing.Point(92, 255);
+            this.textBox_name.Location = new System.Drawing.Point(128, 403);
             this.textBox_name.Name = "textBox_name";
             this.textBox_name.Size = new System.Drawing.Size(134, 26);
             this.textBox_name.TabIndex = 1;
@@ -81,9 +75,9 @@
             // label_name
             // 
             this.label_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label_name.Location = new System.Drawing.Point(12, 258);
+            this.label_name.Location = new System.Drawing.Point(3, 406);
             this.label_name.Name = "label_name";
-            this.label_name.Size = new System.Drawing.Size(78, 20);
+            this.label_name.Size = new System.Drawing.Size(119, 20);
             this.label_name.TabIndex = 4;
             this.label_name.Text = "Имя:";
             this.label_name.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -91,25 +85,27 @@
             // label_message
             // 
             this.label_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label_message.Location = new System.Drawing.Point(12, 284);
+            this.label_message.Location = new System.Drawing.Point(3, 432);
             this.label_message.Name = "label_message";
-            this.label_message.Size = new System.Drawing.Size(78, 20);
+            this.label_message.Size = new System.Drawing.Size(119, 20);
             this.label_message.TabIndex = 4;
             this.label_message.Text = "Сообщение:";
+            this.label_message.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textBox_message
             // 
             this.textBox_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_message.Location = new System.Drawing.Point(92, 281);
+            this.textBox_message.Location = new System.Drawing.Point(128, 429);
             this.textBox_message.Name = "textBox_message";
             this.textBox_message.Size = new System.Drawing.Size(680, 26);
             this.textBox_message.TabIndex = 2;
+            this.textBox_message.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox_message_KeyDown);
             // 
             // button_send
             // 
             this.button_send.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button_send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_send.Location = new System.Drawing.Point(92, 313);
+            this.button_send.Location = new System.Drawing.Point(128, 461);
             this.button_send.Name = "button_send";
             this.button_send.Padding = new System.Windows.Forms.Padding(10, 5, 10, 5);
             this.button_send.Size = new System.Drawing.Size(134, 40);
@@ -118,17 +114,23 @@
             this.button_send.UseVisualStyleBackColor = false;
             this.button_send.Click += new System.EventHandler(this.Button_send_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // MainForm
             // 
+            this.AcceptButton = this.button_send;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.ClientSize = new System.Drawing.Size(823, 511);
             this.Controls.Add(this.label_message);
             this.Controls.Add(this.label_name);
             this.Controls.Add(this.textBox_message);
             this.Controls.Add(this.textBox_name);
-            this.Controls.Add(this.label_informer);
             this.Controls.Add(this.listLabels);
             this.Controls.Add(this.button_send);
             this.Controls.Add(this.button_exit);
@@ -146,12 +148,12 @@
 
         private System.Windows.Forms.Button button_exit;
         private System.Windows.Forms.ListBox listLabels;
-        private System.Windows.Forms.Label label_informer;
         private System.Windows.Forms.TextBox textBox_name;
         private System.Windows.Forms.Label label_name;
         private System.Windows.Forms.Label label_message;
         private System.Windows.Forms.TextBox textBox_message;
         private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ExampleSQLApp/MainForm.cs
+++ ExampleSQLApp/MainForm.cs
@@ -7,82 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
 
 using MySql.Data.MySqlClient;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ExampleSQLApp
 {
     public partial class MainForm : Form
     {
-        private MySqlConnection connection;
+        private string server_url = "http://get.ar";
+        private string server_url_post = "http://get.ar/post.php";
+        private static readonly HttpClient client = new HttpClient();
+
+        private class Message
+        {
+            public string name { get; set; }
+            public string message { get; set; }
+        }
 
 
         public MainForm()
         {
             InitializeComponent();
-            this.connection = DBConnect();
             LoadPosts();
         }
 
-        private MySqlConnection DBConnect()
-        {
-            string connectionParams = "server=localhost;user=root;database=cs_chat;password=;";
-            MySqlConnection connection = new MySqlConnection(connectionParams);
-            connection.Open();
-            label_informer.Text = "DB cs_chat connected";
-            return connection;
-        }
-
-        private void LoadPosts()
+        private async void LoadPosts()
         {
             listLabels.Items.Clear();
-            string sql = "SELECT name, message, created_at FROM messages;";
-            MySqlCommand query = new MySqlCommand(sql, this.connection);
-            MySqlDataReader reader = query.ExecuteReader();
+            HttpResponseMessage response = await client.GetAsync(server_url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Message[] parsed = JsonConvert.DeserializeObject<Message[]>(responseBody);
 
-            while (reader.Read())
+            int count = 0;
+            foreach (Message message in parsed)
             {
                 string title = "";
-                title += reader[0].ToString() + ": " + reader[1].ToString();
+                title += message.name + ": " + message.message;
                 listLabels.Items.Add(title);
+                count++;
             }
-            reader.Close();
+        }
+
+        private async void SendMessage()
+        {
+            if (textBox_name.Text != "" && textBox_message.Text != "")
+            {
+                var message = new Dictionary<string, string>
+                {
+                    { "name", textBox_name.Text },
+                    { "message", textBox_message.Text }
+                };
+
+                textBox_message.Text = "";
+
+                var content = new FormUrlEncodedContent(message);
+                var response = await client.PostAsync(server_url_post, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                LoadPosts();
+            }
         }
 
         private void ButtonExit_Click(object sender, EventArgs e)
         {
-            // Initializes the variables to pass to the MessageBox.Show method.
-            string message = "Не уходи. Останься ещё немного...\nНеужели ты правда хочешь этого?..";
-            string caption = "Выход";
-            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-            DialogResult result;
-
-            // Displays the MessageBox.
-            result = MessageBox.Show(message, caption, buttons);
-            if (result == System.Windows.Forms.DialogResult.Yes)
-            {
-                // Closes the parent form.
-                this.connection.Close();
-                this.Close();
-            }
-            else
-            {
-                
-            }
+            this.Close();
         }
 
-        private void Button_send_Click(object sender, EventArgs e)
+        private async void Button_send_Click(object sender, EventArgs e)
         {
-            if (textBox_name.Text != "" && textBox_message.Text != "")
+            SendMessage();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadPosts();
+        }
+
+        private void textBox_message_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
             {
-                string name = textBox_name.Text;
-                string message = textBox_message.Text;
-                string sql = String.Format(@"INSERT INTO messages (name,message) VALUES ('{0}','{1}');", name, message); ;
-                MySqlCommand query = new MySqlCommand(sql, this.connection);
-                MySqlDataReader reader = query.ExecuteReader();
-                textBox_message.Text = null;
-                reader.Close();
-                LoadPosts();
+                SendMessage();
             }
         }
     }
